416f3352a2705fa8ca1a10eb3ca24bb5610722b4
marmooo <marmooo@users.noreply.github.com>
2020-05-07 22:45:04 +0900

    fix bug

--- build_release.sh
+++ build_release.sh
@@ -1,3 +1,5 @@
-minify --all --recursive photo-scanner --output public
-cp -r photo-scanner/denoise public
+cp -r photo-scanner docs
+minify --all --recursive photo-scanner --output docs
+# cp -r photo-scanner/denoise docs
+# cp -r photo-scanner/img/loading.gif docs/img
 
