8edc324b1f6e41c622d61e1d537c78e79418754f
marmooo <marmooo@users.noreply.github.com>
2022-05-17 00:00:04 +0900

    add OGP

--- src/sw.js
+++ src/sw.js
@@ -1,13 +1,13 @@
-var CACHE_NAME = "2022-05-11 09:49";
+var CACHE_NAME = "2022-05-17 00:00";
 var urlsToCache = [
   "/photo-scanner/js/opencv.js",
   "/photo-scanner/js/index.js",
   "/photo-scanner/js/worker.js",
   "/photo-scanner/img/scanner.svg",
   "/photo-scanner/img/loading.gif",
-  "/photo-scanner/favicon/favicon-48x48.png",
   "/photo-scanner/denoise/model.json",
   "/photo-scanner/denoise/group1-shard1of1.bin",
+  "/photo-scanner/favicon/favicon.svg",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js",
   "https://cdn.jsdelivr.net/npm/dropbox@10.26.0/dist/Dropbox-sdk.min.js",
