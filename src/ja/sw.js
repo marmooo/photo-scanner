229c1500ec290e743e86e3f208134d18fcf9ef56
marmooo <marmooo@users.noreply.github.com>
2022-11-26 13:01:16 +0900

    bump bootstrap from 5.2.2 to 5.2.3

--- src/ja/sw.js
+++ src/ja/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2022-11-13 15:00";
+var CACHE_NAME = "2022-11-26 12:58";
 var urlsToCache = [
   "/photo-scanner/",
   "/photo-scanner/en/",
@@ -10,8 +10,8 @@ var urlsToCache = [
   "/photo-scanner/denoise/model.json",
   "/photo-scanner/denoise/group1-shard1of1.bin",
   "/photo-scanner/favicon/favicon.svg",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js",
   "https://cdn.jsdelivr.net/npm/dropbox@10.26.0/dist/Dropbox-sdk.min.js",
   "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.0.0/dist/tf.min.js",
 ];
