b868db195d21b0184a293bb2cbf04d2f0829a291
marmooo <marmooo@users.noreply.github.com>
2022-11-27 12:06:10 +0900

    bump tfjs from 4.0.0 to 4.1.0

--- src/ja/sw.js
+++ src/ja/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2022-11-26 12:58";
+var CACHE_NAME = "2022-11-27 12:06";
 var urlsToCache = [
   "/photo-scanner/",
   "/photo-scanner/en/",
@@ -13,7 +13,7 @@ var urlsToCache = [
   "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js",
   "https://cdn.jsdelivr.net/npm/dropbox@10.26.0/dist/Dropbox-sdk.min.js",
-  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.0.0/dist/tf.min.js",
+  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.1.0/dist/tf.min.js",
 ];
 
 self.addEventListener("install", function (event) {
