b2f3e3b50adfeabbfa3e3079571e1560feea7a00
marmooo <marmooo@users.noreply.github.com>
2024-03-27 00:05:31 +0900

    bump opencv.js from 4.5.4 to 4.9.0

--- build_opencvjs.sh
+++ build_opencvjs.sh
@@ -1,16 +1,19 @@
-# Version
-#   OpenCV 4.5.4
-#   Emscripten SDK 2.0.31
 base_dir=${HOME}/workspace
-emsdk_dir=${base_dir}/emsdk
-opencv_dir=${base_dir}/opencv
+emsdk_dir=${base_dir}/emsdk  # 2.0.10
+opencv_dir=${base_dir}/opencv-4.9.0
 build_dir=${opencv_dir}/platforms/js
 
-source ${emsdk_dir}/emsdk_env.sh
+source ${emsdk_dir}/emsdk_env.s
 python ${build_dir}/build_js.py ${opencv_dir}/build_wasm --build_wasm \
-  --clean_build_dir \
+  --cmake_option "\
+    -DBUILD_ZLIB=OFF \
+    -DBUILD_opencv_calib3d=OFF \
+    -DBUILD_opencv_dnn=OFF \
+    -DBUILD_opencv_features2d=OFF \
+    -DBUILD_opencv_flann=OFF \
+    -DBUILD_opencv_photo=OFF" \
   --config opencv_js.config_min.py \
+  --clean_build_dir \
   --opencv_dir ${opencv_dir} \
   --emscripten_dir ${emsdk_dir}/upstream/emscripten
 cp ${opencv_dir}/build_wasm/bin/opencv.js src/js
-
