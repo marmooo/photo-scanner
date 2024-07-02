be7d8d23b9fc0526abfe8da7d895cb191d20621b
marmooo <marmooo@users.noreply.github.com>
2024-07-03 01:39:25 +0900

    improve wasm build

--- build_opencvjs.sh
+++ build_opencvjs.sh
@@ -1,22 +1,36 @@
 base_dir=${HOME}/workspace
 emsdk_dir=${base_dir}/emsdk  # 3.1.61
 opencv_dir=${base_dir}/opencv  # 4.10.0
-build_dir=${opencv_dir}/platforms/js
-
-# You can specify the following options:
-  # --simd \
-  # --threads \
-source ${emsdk_dir}/emsdk_env.sh
-python ${build_dir}/build_js.py ${opencv_dir}/build_wasm --build_wasm \
-  --cmake_option "\
-    -DBUILD_ZLIB=OFF \
-    -DBUILD_opencv_calib3d=OFF \
-    -DBUILD_opencv_dnn=OFF \
-    -DBUILD_opencv_features2d=OFF \
-    -DBUILD_opencv_flann=OFF \
-    -DBUILD_opencv_photo=OFF" \
+build_py=${opencv_dir}/platforms/js/build_js.py
+build_dir=${opencv_dir}/build_wasm
+options="\
+  --build_wasm \
+  --cmake_option="-DBUILD_ZLIB=OFF" \
+  --cmake_option="-DBUILD_opencv_calib3d=OFF" \
+  --cmake_option="-DBUILD_opencv_dnn=OFF" \
+  --cmake_option="-DBUILD_opencv_features2d=OFF" \
+  --cmake_option="-DBUILD_opencv_flann=OFF" \
+  --cmake_option="-DBUILD_opencv_photo=OFF" \
+  --cmake_option="-DBUILD_EXAMPLES=OFF" \
   --config opencv_js.config_min.py \
   --disable_single_file \
-  --clean_build_dir \
   --opencv_dir ${opencv_dir} \
-  --emscripten_dir ${emsdk_dir}/upstream/emscripten
+  --emscripten_dir ${emsdk_dir}/upstream/emscripten"
+
+source ${emsdk_dir}/emsdk_env.sh
+
+rm -rf ${build_dir}/bin/*
+python ${build_py} ${build_dir} $options
+cp ${opencv_dir}/build_wasm/bin/* src/opencv/wasm/
+
+rm -rf ${build_dir}/bin/*
+python ${build_py} ${build_dir} $options --simd
+cp ${opencv_dir}/build_wasm/bin/* src/opencv/simd/
+
+rm -rf ${build_dir}/bin/*
+python ${build_py} ${build_dir} $options --threads
+cp ${opencv_dir}/build_wasm/bin/* src/opencv/threads/
+
+rm -rf ${build_dir}/bin/*
+python ${build_py} ${build_dir} $options --simd --threads
+cp ${opencv_dir}/build_wasm/bin/* src/opencv/threaded-simd/
