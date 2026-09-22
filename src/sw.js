9cc8848df152421fe064634808f1f9b401f6e766
marmooo <marmooo@users.noreply.github.com>
2026-09-22 15:23:14 +0900

    bump wasm-feature-detect from 1.8.0 to 1.9.0

--- src/sw.js
+++ src/sw.js
@@ -1,15 +1,15 @@
-const cacheName = "2026-06-11 00:00";
+const cacheName = "2026-09-22 00:00";
 const urlsToCache = [
   "/photo-scanner/index.js",
   "/photo-scanner/img/scanner.svg",
   "/photo-scanner/img/loading.gif",
   "/photo-scanner/camera.mp3",
   "/photo-scanner/favicon/favicon.svg",
-  "https://cdn.jsdelivr.net/npm/wasm-feature-detect@1.8.0/dist/umd/index.min.js",
+  "https://cdn.jsdelivr.net/npm/wasm-feature-detect@1.9.0/dist/umd/index.min.js",
 ];
 
 importScripts(
-  "https://cdn.jsdelivr.net/npm/wasm-feature-detect@1.8.0/dist/umd/index.min.js",
+  "https://cdn.jsdelivr.net/npm/wasm-feature-detect@1.9.0/dist/umd/index.min.js",
 );
 
 async function getOpenCVPath() {
